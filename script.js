88a58f965ccc65be332b4e6c8798be76613abe4f
BarcosyPizzas <67290371+BarcosyPizzas@users.noreply.github.com>
2024-03-13 20:58:08 -0700

    Soy debil
    Debil

---
+++ script.js
@@ -0,0 +1,104 @@
+let characters = [
+    {
+        Nombre: "Phil Dunphy",
+        Sexo: "h",
+        Edad: "43",
+        Pelo: "Rubio",
+        Familia: "Dumphy",
+        Ocupacion: "Agencia Inmobiliaria",
+        esPapa: "1"
+    },
+    {
+        Nombre: "Claire Dunphy",
+        Sexo: "m",
+        Edad: "43",
+        Pelo: "Rubio",
+        Familia: "Dumphy",
+        Ocupacion: "CEO de prichetts closets",
+        esPapa: "1"
+    },
+    {
+        Nombre: "Haley Dunphy",
+        Sexo: "m",
+        Edad: "26",
+        Pelo: "Castaño",
+        Familia: "Dumphy",
+        Ocupacion: "Trabajadora de Nerp",
+        esPapa: "1"
+    },
+    {
+        Nombre: "Alex Dunphy",
+        Sexo: "m",
+        Edad: "23",
+        Pelo: "Castaño",
+        Familia: "Dumphy",
+        Ocupacion: "Investigadora Cientifica",
+        esPapa: "0"
+    },
+    {
+        Nombre: "Luke Dunphy",
+        Sexo: "h",
+        Edad: "20",
+        Pelo: "Castaño",
+        Familia: "Dumphy",
+        Ocupacion: "Estudiante de universidad",
+        esPapa: "0"
+    },
+    {
+        Nombre: "Mitch Pritchett",
+        Sexo: "h",
+        Edad: "43",
+        Pelo: "Pelirojo",
+        Familia: "Pritchett",
+        Ocupacion: "Abogado",
+        esPapa: "1"
+    },
+    {
+        Nombre: "Cam Tucker",
+        Sexo: "h",
+        Edad: "43",
+        Pelo: "Negro",
+        Familia: "Tucker",
+        Ocupacion: "Maestro",
+        esPapa: "1"
+    },
+    {
+        Nombre: "Lily",
+        Sexo: "f",
+        Edad: "13",
+        Pelo: "Negro",
+        Familia: "Tucker-Pritchett",
+        Ocupacion: "Alumno",
+        esPapa: "0"
+    },
+    {
+        Nombre: "Gloria Pritchett",
+        Sexo: "f",
+        Edad: "40",
+        Pelo: "Cafe",
+        Familia: "Delgado-Pritchett",
+        Ocupacion: "Casa",
+        esPapa: "1"
+    },
+    {
+        Nombre: "Jay Pritchett",
+        Sexo: "h",
+        Edad: "70",
+        Pelo: "Negro",
+        Familia: "Pritchett",
+        Ocupacion: "Emprendedor",
+        esPapa: "1"
+    },
+    {
+        Nombre: "Manny Delgado",
+        Sexo: "h",
+        Edad: "21",
+        Pelo: "Negro",
+        Familia: "Delgado-Pritchett",
+        Ocupacion: "Alumno",
+        esPapa: "0"
+    }
+]
+
+let preguntas = ["¿Tu personaje es mujer?","¿Tu personaje es mayor a 30?","¿Tu personaje es mayor a 30?","¿Tu personaje es de la familia Dunphy?","¿Tu personaje es de la Familia Dunphy?","¿Tu perosnaje es de la familia Tucker?","¿Tu perosnaje es de la familia Dunphy?"," "," ","¿Tu personaje tiene hijos?"," ","¿Tu personaje es pelirrojo?","¿Tu personaje trabaja en una inmobiliaria?"," "," "]
+
